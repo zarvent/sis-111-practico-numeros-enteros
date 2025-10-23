707524d7c6d26619c043f74e1800896607f270ad
zarvent <zarventt@gmail.com>
2025-10-23 17:53:17 -0400

    Add initial implementation of programming exercises and instructions

---
+++ numeros enteros.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+/*/
+LOS PROTOTIPOS DE FUNCION
+*/
+int valorAbsoluto(int numero);
+bool comparativa(int numerosUnicos, int numeroCandidato, );
+long long potencia(int base, int exponente);
+
+
+/*
+EL MAIN
+*/
+int main(){
+
+    return 0;
+}
+
+
+/*
+FUNCIONES PAPETO
+*/
+int valorAbsoluto(int numero){
+    /*
+    Aqui usare un operador tercinario
+    basicamente funciona asi:
+    condicion ? expresion1: expresion2;
+    osea
+    pregunta ? valorPorVerdad: valorPorMentira;
+
+    lo que hara mi funcion es que se preguntara si el numero es menor que 0, si es menor que 0 lo convertirremos a positivo y si es positivo lo dejamos como esta
+    */
+    return (n < 0) ? -numero : numero;
+}
+int contarDiferentes(int numero, int cantidadDigitos){
+}
+bool comparativa(int numerosUnicos, int numeroCandidato, ){
+    while(numerosUnicos > 0){
+        ultimoDigito = numerosUnicos % 10;
+        if(ultimoDigito == numeroCandidato){
+            return false;
+    }
+}
+return true;
+}
+int numerosUnicos(int numero){
+    int numeroInicial = numero;
+    int numerosUnicos = 0;
+    while(numeroInicial > 0){
+        // sera el ultimo digito de numero inicial
+        int numeroCandidato = numeroInicial % 10;
+        // utilizare la comparativa
+        // si la comparativa me da true (osea que si es unico ejecutara......)
+        if(comparativa(numeroUnicos, numeroCandidato) == true){
+            //exponente potencia lo que hace es saber cuantas veces tengo que hacer el caluclo para armar el numero unico
+            int exponentePotencia = 0;
+            while(numerosUnicos > 0){
+                exponentePotencia++;
+                // si empiezo en 3
+                numerosUnicos / 10;
+            }
+            numerosUnicos = numerosUnicos + (numeroCandidato * pow(10, exponente))
+        }
+    }
+}
+
+long long
+
+long long potencia(int base, int exponente){
+
+}
