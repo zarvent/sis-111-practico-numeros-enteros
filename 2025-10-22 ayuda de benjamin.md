0665f0707358c08f69c32d9deecdd18f95a4c92c
zarvent <zarventt@gmail.com>
2025-10-24 14:31:37 -0400

    Refactor code structure by removing unused functions and adding function definitions for unique number calculations

---
+++ 2025-10-22 ayuda de benjamin.md
@@ -0,0 +1,37 @@
+
+/*
+AYUDA DE BENJAMIN
+int contarDiferentes(int numero, int cantidadDigitos){
+}
+
+bool comparativa(int numerosUnicos, int numeroCandidato, ){
+    while(numerosUnicos > 0){
+        ultimoDigito = numerosUnicos % 10;
+        if(ultimoDigito == numeroCandidato){
+            return false;
+    }
+}
+return true;
+}
+int numerosUnicos(int numero){
+    int numeroInicial = numero;
+    int numerosUnicos = 0;
+    while(numeroInicial > 0){
+        // sera el ultimo digito de numero inicial
+        int numeroCandidato = numeroInicial % 10;
+        // utilizare la comparativa
+        // si la comparativa me da true (osea que si es unico ejecutara......)
+        if(comparativa(numeroUnicos, numeroCandidato) == true){
+            //exponente potencia lo que hace es saber cuantas veces tengo que hacer el caluclo para armar el numero unico
+            int exponentePotencia = 0;
+            while(numerosUnicos > 0){
+                exponentePotencia++;
+                // si empiezo en 3
+                numerosUnicos / 10;
+            }
+            numerosUnicos = numerosUnicos + (numeroCandidato * pow(10, exponente))
+        }
+    }
+}
+
+/*
--- numeros enteros.cpp
+++ numeros enteros.cpp
@@ -1,3 +1,4 @@
+//DIRECTIVAS DEL PREPROCESADOR
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -19,7 +20,7 @@ int main(){
 }
 */
 
-
+// DEFINICIONES DE FUNCION
 int ultimoDigito(int numero){
     return abs(numero % 10);
 }
@@ -86,43 +87,3 @@ int cantidadDeDigitos(int numero){
     }
        return cantidadDigitos;
 }
-
-
-
-
-/*
-AYUDA DE BENJAMIN
-int contarDiferentes(int numero, int cantidadDigitos){
-}
-
-bool comparativa(int numerosUnicos, int numeroCandidato, ){
-    while(numerosUnicos > 0){
-        ultimoDigito = numerosUnicos % 10;
-        if(ultimoDigito == numeroCandidato){
-            return false;
-    }
-}
-return true;
-}
-int numerosUnicos(int numero){
-    int numeroInicial = numero;
-    int numerosUnicos = 0;
-    while(numeroInicial > 0){
-        // sera el ultimo digito de numero inicial
-        int numeroCandidato = numeroInicial % 10;
-        // utilizare la comparativa
-        // si la comparativa me da true (osea que si es unico ejecutara......)
-        if(comparativa(numeroUnicos, numeroCandidato) == true){
-            //exponente potencia lo que hace es saber cuantas veces tengo que hacer el caluclo para armar el numero unico
-            int exponentePotencia = 0;
-            while(numerosUnicos > 0){
-                exponentePotencia++;
-                // si empiezo en 3
-                numerosUnicos / 10;
-            }
-            numerosUnicos = numerosUnicos + (numeroCandidato * pow(10, exponente))
-        }
-    }
-}
-
-/*
